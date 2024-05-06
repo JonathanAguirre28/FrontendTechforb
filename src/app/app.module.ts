473a6b23f7e279f087ea7c59ae35e6f933427cc4
JonathanAguirre <jonathan621924@gmail.com>
2024-05-05 23:06:33 -0300

    iniciando proyecto, generacion de componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardsComponent } from './components/cards/cards.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { TableComponent } from './components/table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardsComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidenavComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
